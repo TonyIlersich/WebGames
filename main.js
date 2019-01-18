91ba8e23ca048197586a85978a13ac040c1d49aa
Tony Ilersich <antonio.ilersich@gmail.com>
2019-01-18 16:40:47 -0500

    updated main.js
    made simple time-based animation on background canvas

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
+const TAU = 2 * Math.PI;
+
+bg = {
+	FPS: 1,
+	width: undefined,
+	height: undefined,
+	canvas: null,
+	context: null,
+	
+	init: function()
+	{
+		this.width = $("#bgCanvas").attr("width");
+		this.height = $("#bgCanvas").attr("height");
+		this.canvas = document.getElementById("bgCanvas");
+		this.context = this.canvas.getContext("2d");
+		setInterval(() => { this.redraw(); }, 1000 / this.FPS);
+	},
+	
+	redraw: function()
+	{
+		this.context.strokeStyle = "#00ff00";
+		this.context.beginPath();
+		this.context.arc(this.width / 2, this.height / 2, 0, Date.now() * TAU);
+		this.context.stroke();
+	},
+	
+	
+}
+
 $(document).ready(function ()
 {
 	console.log("page loaded");
+	
+	bg.init();
 });
