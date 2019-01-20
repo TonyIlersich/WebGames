06869ac9f20de0a9dc888f9cd3af90d1bf55d802
Tony <antonio.ilersich@gmail.com>
2019-01-20 13:26:56 -0500

    tweak circuit code, added scrollbar, added comments

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const TAU = 2 * Math.PI;
+const PHI = 1.61803398875;
 
 bg = {
 	FPS: 24,
@@ -8,10 +9,13 @@ bg = {
 	canvas: null,
 	context: null,
 	tick: undefined,
-	fractal: null,
+	circuit: null,
+	meter: null,
 	
-	Fractal: function(x0, y0, x1, y1)
+	//this 
+	Circuit: function(x0, y0, x1, y1)
 	{
+		//these properties are used for spacing
 		const WIDTH = x1 - x0;
 		const HEIGHT = y1 - y0;
 		const MARGIN = .1 * WIDTH;
@@ -19,80 +23,81 @@ bg = {
 		var BRANCH_WIDTH = undefined;
 		const N_SEGMENTS = 30;
 		const SEGMENT_HEIGHT = (HEIGHT - 2 * MARGIN) / N_SEGMENTS;
-		const OPTIONS_FROM_START_NODE = [
-			"continue"
-		];
-		const OPTIONS_FROM_END_NODE = [
-			"dead"
-		];
-		const OPTIONS_FROM_LINE = [
-			"continue",
-			"shift",
-			"end node"
-		];
-		const OPTIONS_FROM_DEAD = [
-			"dead",
-			"start node"
-		];
+		const OPTIONS = {
+			"start node": [ "continue" ],
+			"end node": [ "dead" ],
+			"line": [ "continue", "shift", "end node" ],
+			"dead": [ "dead", "start node" ]
+		};
 		
+		//this function recalculates some constants to address a change in aspect ratio
 		this.refresh = function()
 		{
 			N_BRANCHES = Math.floor(bg.aspect * 4);
 			BRANCH_WIDTH = (WIDTH - 2 * MARGIN) / (N_BRANCHES - 1);
+			if (this.branchState)
+			{
+				while (this.branchState.length < N_BRANCHES)
+				{
+					this.branchState.push("start node");
+				}
+			}
 		}
 		
 		this.refresh();
 		
+		//this function calculates the x-position of the i-th branch
 		this.branchX = function(i)
 		{
 			return MARGIN + i * BRANCH_WIDTH;
 		}
 		
+		//this property keeps track of what state we left each branch in
 		this.branchState = [];
 		for (var i = 0; i < N_BRANCHES; i++)
 		{
 			this.branchState.push("start node");
 		}
 		
+		//this property keeps track of which segment we are on
 		this.segment = 0;
 		
+		//this function selects a random option given a the state a branch is in
+		this.randomOption = function(state)
+		{
+			var i = Math.floor(Math.random() * OPTIONS[state].length);
+			return OPTIONS[state][i];
+		}
+		
+		//this function draws the next segment in the animation
 		this.step = function()
 		{
+			//set the correct context settings
 			bg.context.lineCap = "round";
 			bg.context.lineWidth = Math.min(.5 * MARGIN, .2 * BRANCH_WIDTH);
 			
+			//check that we would draw on-screen
 			if (this.segment >= 0)
 			{
+				//assume each branch will be dead in the next step
 				var futureState = [];
-				
 				for (var i = 0; i < N_BRANCHES; i++)
 				{
 					futureState[i] = "dead";
 				}
 				
+				//cycle through all branches
 				for (var i = 0; i < N_BRANCHES; i++)
 				{
+					//choose an appropriate action for how to proceed
 					var action = "error";
-					
 					if (this.segment == N_SEGMENTS)
 					{
 						action = "end node";
 					}
-					else if (this.branchState[i] == "start node")
+					else
 					{
-						action = OPTIONS_FROM_START_NODE[Math.floor(Math.random() * OPTIONS_FROM_START_NODE.length)];
-					}
-					else if (this.branchState[i] == "end node")
-					{
-						action = OPTIONS_FROM_END_NODE[Math.floor(Math.random() * OPTIONS_FROM_END_NODE.length)];
-					}
-					else if (this.branchState[i] == "line")
-					{
-						action = OPTIONS_FROM_LINE[Math.floor(Math.random() * OPTIONS_FROM_LINE.length)];
-					}
-					else if (this.branchState[i] == "dead")
-					{
-						action = OPTIONS_FROM_DEAD[Math.floor(Math.random() * OPTIONS_FROM_DEAD.length)];
+						action = this.randomOption(this.branchState[i]);
 					}
 					
 					if (action == "error")
@@ -101,18 +106,23 @@ bg = {
 					}
 					else if (action == "start node" || action == "end node")
 					{
+						//get the position of the node on the canvas
 						var x = this.branchX(i);
-						var y = this.segment * SEGMENT_HEIGHT + MARGIN;
+						var y;
 						
 						if (action == "start node")
 						{
 							y = (this.segment + 1) * SEGMENT_HEIGHT + MARGIN;
 						}
+						else
+						{
+							y = this.segment * SEGMENT_HEIGHT + MARGIN;
+						}
 						
+						//draw circle to canvas
+						var radius = Math.min(MARGIN, .4 * BRANCH_WIDTH);
 						bg.context.fillStyle = bg.clr(0, .5, 1, 1);
 						bg.context.beginPath();
-						var radius = Math.min(MARGIN, .4 * BRANCH_WIDTH);
-						console.log(radius * bg.aspect);
 						bg.context.ellipse(x, y, radius, radius * bg.aspect, 0, 0, TAU);
 						bg.context.fill();
 
@@ -188,6 +198,105 @@ bg = {
 		}
 	},
 	
+	Meter: function()
+	{
+		const WIDTH = .1 * bg.width;
+		const HEIGHT = bg.height;
+		const MARGIN = .1 * WIDTH;
+		
+		const N_NOTCHES = 61;
+		
+		this.notchY = function(i)
+		{
+			var progress = i / (N_NOTCHES - 1);
+			var usefulHeight = HEIGHT - 2 * MARGIN;
+			return MARGIN + usefulHeight * progress;
+		}
+		
+		this.redraw = function()
+		{
+			bg.context.lineCap = "round";
+			bg.context.lineWidth = .25 * MARGIN;
+			bg.context.strokeStyle = bg.clr(0, .5, 1, 1);
+			
+			for (var i = 0; i < N_NOTCHES; i++)
+			{
+				var usefulWidth = WIDTH - 2 * MARGIN;
+				
+				var x0 = bg.width - usefulWidth * .5 - MARGIN;
+				var x1 = bg.width - usefulWidth * .375 - MARGIN;
+				var x2 = bg.width - usefulWidth * .25 - MARGIN;
+				var x3 = bg.width - usefulWidth * .125 - MARGIN;
+				var x4 = bg.width - MARGIN;
+				
+				var xl = (i % 10 == 0) ? x0 : x1;
+				var xr = (i % 10 == 0) ? x4 : x3;
+				
+				bg.context.beginPath()
+				bg.context.moveTo(x2, this.notchY(0));
+				bg.context.lineTo(x2, this.notchY(N_NOTCHES - 1));
+				bg.context.stroke();
+				
+				var y0 = this.notchY(i);
+				var y1 = this.notchY(i * PHI);
+				
+				bg.context.shadowColor = bg.clr(0, .5, 1, .5);
+				bg.context.shadowBlur = 5 * bg.context.lineWidth;
+				
+				bg.context.beginPath();
+				bg.context.moveTo(x2, y0);
+				bg.context.lineTo(xl, y0);
+				bg.context.stroke();
+				
+				bg.context.beginPath();
+				bg.context.moveTo(x2, y1);
+				bg.context.lineTo(xr, y1);
+				bg.context.stroke();
+				
+				bg.context.shadowBlur = 0;
+			}
+		}
+		
+		this.drawPointer = function()
+		{
+			var elem = $("#gameList");
+			
+			var usefulHeight = HEIGHT - 2 * MARGIN;
+			var yScroll = elem.scrollTop();
+			var yMax = elem[0].scrollHeight - elem.height();
+			var y = usefulHeight * yScroll / yMax;
+			
+			var pointerHeight = .02 * usefulHeight;
+			var y0 = y - pointerHeight / 2;
+			var y1 = y + pointerHeight / 2;
+			
+			var usefulWidth = WIDTH - 2 * MARGIN;
+			var x0 = bg.width - usefulWidth - MARGIN;
+			var x1 = bg.width - usefulWidth * .65 - MARGIN;
+			
+			bg.context.lineCap = "square";
+			bg.context.lineWidth = .25 * MARGIN;
+			bg.context.fillStyle = bg.clr(0, .5, 1, .2);
+			
+			bg.context.beginPath();
+			bg.context.moveTo(x0, y0);
+			bg.context.lineTo(x1, y);
+			bg.context.lineTo(x0, y1);
+			bg.context.lineTo(x0, y0);
+			bg.context.closePath();
+			bg.context.fill();
+			bg.context.stroke();
+			
+			console.log(x0 + ", " + y);
+		}
+		
+		this.update = function()
+		{
+			this.redraw();
+			this.drawPointer();
+		}
+	},
+	
 	clr: function(r, g, b, a)
 	{
 		return "rgba(" + r * 255 + "," + g * 255 + "," + b * 255 + "," + a + ")";
@@ -209,7 +318,8 @@ bg = {
 		this.canvas = document.getElementById("bgCanvas");
 		this.context = this.canvas.getContext("2d");
 		
-		this.fractal = new this.Fractal(0, 0, .1 * this.width, this.height);
+		this.circuit = new this.Circuit(0, 0, .1 * this.width, this.height);
+		this.meter = new this.Meter();
 		
 		this.tick = 0;
 		
@@ -222,7 +332,8 @@ bg = {
 		
 		this.fade(this.clr(0, .05, .1, .1));
 		
-		this.fractal.step();
+		this.circuit.step();
+		this.meter.update();
 		
 		this.tick++;
 	},
@@ -238,7 +349,7 @@ $(document).ready(function ()
 {
 	console.log("page loaded");
 	
-	$(window).resize(() => { bg.getAspect(); bg.fractal.refresh(); });
+	$(window).resize(() => { bg.getAspect(); bg.circuit.refresh(); });
 	
 	bg.init();
 });
